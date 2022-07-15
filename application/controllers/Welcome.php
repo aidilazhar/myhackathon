8e1b497af78062ab71e4ff91008b0a2bb47fed33
Aidil Azhar <aidilazhar@me.com>
2022-07-15 10:45:48 +0800

    updates

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,36 +20,79 @@ class Welcome extends CI_Controller {
 	 * 
 	 */
 
-	public function __construct()
-	{
-		parent::__construct();
-		$this->load->helper(array('url', 'html'));
-		$this->load->library(array('session', 'form_validation'));
-		$this->load->library('session');
-		
-
-	    }
+	 
 	public function index()
 	{
 		$this->load->view('welcome_message');
 	}
-
-	public function index1()
-	{
-		$this->load->view('welcome_message1');
-	}
-
+   
 	public function formpost()
-{
+    {
     
-	print_r($this->input->post('name'));
-    print_r($this->input->post('email'));
-   
-    print_r($this->input->post());
-   
-    print_r($_POST['name']);
-    print_r($_POST['email']);
-    print_r($_POST);
+	echo $this->input->post('searchid');
+	echo $this->input->post('carid');
+	echo $this->input->post('make');
+	echo $this->input->post('model');
+	echo $this->input->post('colour');
+	echo $this->input->post('owner');
+	echo $this->input->post('image');
+	echo $this->input->post('video');
+	
+	
+	$config = array(
+		'upload_path' => APPPATH ."uploads/",
+		'allowed_types' => "gif|jpg|png|jpeg|pdf",
+		'overwrite' => TRUE,
+		'max_size' => "2048000", // Can be set to particular file size , here it is 2 MB(2048 Kb)
+		'max_height' => "768",
+		'max_width' => "1024"
+		);
+		$this->load->library('upload', $config);
+		if($this->upload->do_upload())
+		{
+		$data = array('upload_data' => $this->upload->data());
+		}
+		else
+		{
+		$error = array('error' => $this->upload->display_errors());
+        print_r($error);
+		}
+		
+	
+	die;
+
+
+
+	$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'http://139.59.124.53:8080/api/addcar',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS =>'{
+	"carid": "CAR100",
+	"make": "Honda",
+	"model": "Accord",
+	"colour": "black",
+	"owner": "Tom",
+	"image": "Tom",
+	"video": "Tom",
+}',
+  CURLOPT_HTTPHEADER => array(
+    'Content-Type: application/json'
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
+
     die;
-}
+    }
 }
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,14 +1,144 @@
-<form action="index.php?formpost" method="POST" enctype="multipart/form-data">
-  
-    <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="email">
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+ 
+
+    <title>GrowHub BlockChain</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/checkout/">
+
+
+  </head>
+
+  <body class="bg-light">
+
+    <div class="container">
+      <div class="py-5 text-center">
+        <h2>GrowHub BlockChain</h2>
+     </div>
+
+      <div class="row">
+        <div class="col-md-4 order-md-2 mb-4">
+          <h4 class="d-flex justify-content-between align-items-center mb-3">
+            <span class="text-muted">Result</span>
+            <span class="badge badge-secondary badge-pill">3</span>
+          </h4>
+          <ul class="list-group mb-3">
+           
+            <li class="list-group-item d-flex justify-content-between bg-light">
+              <div class="text-success">
+              
+                <small>Success</small>
+              </div>
+       
+            </li>
+            
+          </ul>
+		 
+        </div>
+        <div class="col-md-8 order-md-1">
+		<h4 class="mb-3">Test Application</h4>
+		<form action="index.php/welcome/formpost" method="POST" enctype="multipart/form-data">
+
+		<div class="form-check">
+             <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+             <label class="form-check-label" for="flexRadioDefault1">Query</label>
+             </div>
+        <div class="form-check">
+             <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+             <label class="form-check-label" for="flexRadioDefault2">Create</label>
+            </div>
+			<div class="form-check">
+             <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+             <label class="form-check-label" for="flexRadioDefault2">Update</label>
+            </div>
+            <hr class="mb-4">
+
+
+
+			<div class="mb-3">
+        
+              <input type="text" class="form-control" id="searchid" name="searchid" placeholder="Search ID" required>
+              <div class="invalid-feedback">
+                Please enter your Car ID.
+              </div>
+            </div>
+<hr class="mb-4">
+       
+            <div class="row">
+              <div class="col-md-6 mb-3">
+                <input type="text" class="form-control" id="carid" name="carid" placeholder="CAR ID" value="" required>
+                <div class="invalid-feedback">
+				Valid CARID is required.
+                </div>
+              </div>
+              <div class="col-md-6 mb-3">
+                <input type="text" class="form-control" id="owner" placeholder="Owner" value="" required>
+                <div class="invalid-feedback">
+                  Valid Owner is required.
+                </div>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-4 mb-4">
+                <label for="country">Make</label>
+                <select class="custom-select d-block w-100" id="make" name="make" required>
+                  <option value="">Choose...</option>
+                  <option value="honda">Honda</option>
+                </select>
+                <div class="invalid-feedback">
+                  Please select a valid country.
+                </div>
+              </div>
+              <div class="col-md-4 mb-4">
+                <label for="state">Model</label>
+                <select class="custom-select d-block w-100" id="model" name="model" required>
+                  <option value="">Choose...</option>
+                  <option value="civic">Civic</option>
+                </select>
+                <div class="invalid-feedback">
+                  Please provide a valid Model
+                </div>
+              </div>
+
+			  <div class="col-md-4 mb-4">
+                <label for="state">Colour</label>
+                <select class="custom-select d-block w-100" id="colour" name="colour" required>
+                  <option value="">Choose...</option>
+                  <option value="black">Black</option>
+                </select>
+                <div class="invalid-feedback">
+                  Please provide a valid state.
+                </div>
+              </div>
+      
+			<div class="mb-4">
+               <label for="formFileSm" class="form-label">Image or Video</label>
+               <input class="form-control form-control-sm" id="formFileSm" type="file" name='userfile'>
+            </div>
+            <div>
+
+			<hr class="mb-4">
+
+			<div><input type="submit" value="Submit" /></div>
+
+			</form>
+
+ 
     </div>
-   
-    <div class="form-group">
-        <label for="exampleInputEmail1">Name</label>
-        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Name" name="name">
-    </div>
-  
-    <button class="btn btn-success">Save</button>
-</form>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+</html>
