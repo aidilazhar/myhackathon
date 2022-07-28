ff693cf3a70630aca694d7144579dd9a22f4c0a0
Aidil Azhar <aidilazhar@me.com>
2022-07-28 10:20:59 +0800

    updates

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -55,7 +55,7 @@
       <div class="row">
       <?php echo $this->session->flashdata('message_name');  ?>
         <div class="col-md-12 order-md-1">
-		<h4 class="mb-3">Test Application</h4>
+		<h4 class="mb-3">Blockchain Application</h4>
 		<form action="index.php/welcome/" method="POST" enctype="multipart/form-data">
 
 		<div class="form-check">
@@ -85,7 +85,7 @@
        
             <div class="row">
               <div class="col-md-6 mb-3">
-                <input type="text" class="form-control" id="carid" name="carid" placeholder="CAR ID"  >
+                <input type="text" class="form-control" id="carid" name="carid" placeholder="Batch ID"  >
                 <div class="invalid-feedback">
 				Valid CARID is required.
                 </div>
@@ -104,8 +104,8 @@
                 <select class="custom-select d-block w-100" id="make" name="make" >
                   <option value="">Choose...</option>
                   <option value="honda">Honda</option>
-				  <option value="audi">Audi</option>
-				  <option value="bmw">BMW</option>
+				  <option value="1">Honey 1</option>
+				  <option value="2">Honey 2</option>
                 </select>
                 <div class="invalid-feedback">
                   Please select a valid country.
@@ -152,6 +152,49 @@
 
  
     </div>
+    <script src="http://maps.google.com/maps/api/js?key=AIzaSyCmvdjPpeWpjDzVY2YXSK16z2NtZxnT_-k" 
+          type="text/javascript"></script>
+</head> 
+<body>
+  <div id="map" style="width: 500px; height: 400px;"></div>
+
+  <script type="text/javascript">
+    var locations = [
+      ['Bondi Beach', -33.890542, 151.274856, 4],
+      ['Coogee Beach', -33.923036, 151.259052, 5],
+      ['Cronulla Beach', -34.028249, 151.157507, 3],
+      ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+      ['Maroubra Beach', -33.950198, 151.259302, 1]
+    ];
+    
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: new google.maps.LatLng(-33.92, 151.25),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+    
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+    
+    for (i = 0; i < locations.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+      
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
+  </script>
+
+
+
+
 
 
   <nav class="navbar navbar-expand-lg fixed-bottom navbar-dark bg-primary">
