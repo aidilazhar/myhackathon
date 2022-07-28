72923536d85bd1bb2c3c74aa34d8cc4aec48911a
Aidil Azhar <aidilazhar@me.com>
2022-07-28 12:29:07 +0800

    updates

--- application/views/action.php
+++ application/views/action.php
@@ -176,11 +176,7 @@ $jsondata = json_decode($jsondata['response'], true);
 
   <script type="text/javascript">
     var locations = [
-      ['Bondi Beach', <?php echo $jsondata['model']; ?> ],
-      ['Coogee Beach', -33.923036, 151.259052, 5],
-      ['Cronulla Beach', -34.028249, 151.157507, 3],
-      ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
-      ['Maroubra Beach', -33.950198, 151.259302, 1]
+      ['Bondi Beach', <?php echo $jsondata['model']; ?> ]
     ];
     
     var map = new google.maps.Map(document.getElementById('map'), {
