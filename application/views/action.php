c2eaef255ff6ac2e97a89f0c20bb75a9696b3532
Aidil Azhar <aidilazhar@me.com>
2022-07-28 12:24:10 +0800

    updates

--- application/views/action.php
+++ application/views/action.php
@@ -54,14 +54,14 @@
 <div>Results
 <div class="alert alert-primary" role="alert">
 <?php 
-
+if(isset($result)){
 $result = $this->session->flashdata('message_name'); 
 echo $result;
 
 $jsondata = json_decode($result, true);
 $jsondata = json_decode($jsondata['response'], true);
-echo $jsondata['model'];
 
+}
 
 ?>
 </div>
@@ -174,11 +174,8 @@ echo $jsondata['model'];
 
   <script type="text/javascript">
     var locations = [
-      ['Bondi Beach', -33.890542, 151.274856, 4],
-      ['Coogee Beach', -33.923036, 151.259052, 5],
-      ['Cronulla Beach', -34.028249, 151.157507, 3],
-      ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
-      ['Maroubra Beach', -33.950198, 151.259302, 1]
+      ['Bondi Beach', <?php echo $jsondata['model']; ?>]
+
     ];
     
     var map = new google.maps.Map(document.getElementById('map'), {
